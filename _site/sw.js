b5051640316474e4d749f2743e96159348c898ea
Borislav <b.dostumski@gmail.com>
2023-08-26 14:47:31 +0300

    favicon

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230826.144545';
+const cacheName = 'chirpy-20230826.144627';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
